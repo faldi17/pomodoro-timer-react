85f489f80149532400c280c35ac1475cf81d8156
Faldiansyah <faldiansyahk@gmail.com>
2025-03-11 17:38:18 +0700

    add modals

--- src/components/StateProvider.jsx
+++ src/components/StateProvider.jsx
@@ -4,7 +4,7 @@ export const StateContext = createContext()
 
 const StateProvider = ({ children }) => {
     const [workTime, setWorkTime] = useState(25 * 60)
-    const [shortBreakTime, setShortBreakTime] = useState(3)
+    const [shortBreakTime, setShortBreakTime] = useState(5 * 60)
     const [longBreakTime, setLongBreakTime] = useState(30 * 60)
 
     const [initTime, setInitTime] = useState(0)
